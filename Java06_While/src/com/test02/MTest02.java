105b1c66320cc59d1c87ce997a1122a741db6a82
printf100 <mickey0502@naver.com>
2019-11-07 19:24:29 +0900

    20191107 break, continue

---
+++ Java06_While/src/com/test02/MTest02.java
@@ -0,0 +1,24 @@
+package com.test02;
+
+public class MTest02 {
+
+	public static void main(String[] args) {
+		prn();
+	}
+	
+	public static void prn() {
+		
+		int i = 0;
+		
+		while(i <= 10) {
+
+			i++;
+			
+			if(i % 2 == 0) {
+				continue;	// 해당 반복문의 제일 위로 올라감
+			}
+			
+			System.out.println(i);
+		}
+	}
+}
